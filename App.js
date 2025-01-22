58ae9a07f5fc14f8ed7d01a389bad8660814c2dc
Weatherserin <erin.weathers@student.uagc.edu>
2025-01-21 19:02:52 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import EmployeeForm from './Components/EmployeeForm.js';
+
+function App() {
+  return (
+		<EmployeeForm />
+  );
+}
+
+export default App;
